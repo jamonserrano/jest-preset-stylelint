d6eec3817e3eadd860e6c28bfae48c56413d1f72
Aleks Hudochenkov <aleks@hudochenkov.com>
2020-05-10 00:27:26 +0200

    Remove stylelint

--- jest-setup.js
+++ jest-setup.js
@@ -3,6 +3,5 @@
 jest.mock('stylelint/lib/utils/getOsEol', () => () => '\n');
 
 const getTestRule = require('./getTestRule');
-const stylelint = require('stylelint');
 
-global.testRule = getTestRule(stylelint);
+global.testRule = getTestRule();
